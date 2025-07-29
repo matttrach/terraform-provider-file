4161a9a8d8f791d642a0d4cede8d355d3c29d395
Matt Trachier <matt.trachier@suse.com>
2025-07-29 13:23:31 -0500

    fix: add release automation (#7)
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- examples/resources/file_local/import.sh
+++ examples/resources/file_local/import.sh
@@ -1,4 +1,8 @@
 # Copyright (c) HashiCorp, Inc.
 
+# echo "Test data" > data.txt
+# FILEPATH="./data.txt"
+# SECRET="super-secret-key"
+# IDENTIFIER="$(openssl dgst -sha256 -hmac "$SECRET" "$FILE" | awk '{print $2}')"
 
-terraform import file_local "id-123"
+terraform import file_local "IDENTIFIER"
